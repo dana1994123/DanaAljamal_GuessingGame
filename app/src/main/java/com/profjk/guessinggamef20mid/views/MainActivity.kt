68a2f40bf3c754a953364e80e90a49d3c41d7817
dana <danaaljamal94@gmail.com>
2020-10-22 09:53:03 -0400

    Check match Numberr

--- app/src/main/java/com/profjk/guessinggamef20mid/views/MainActivity.kt
+++ app/src/main/java/com/profjk/guessinggamef20mid/views/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.profjk.guessinggamef20mid.R
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener  {
 
 
         //generate a random number and save it so user ca guess wtis it :
+        this.generateRandomNumber()
 
         //allow 5 attempts to the user to see if it is right
     }
@@ -41,13 +44,44 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener  {
     //when the btn is clicked we want to check if the num = random and validate
     override fun onClick(v: View?) {
         if (v != null) {
-            if (v.id == edtAnswer.id){
+            if (v.id == edtAnswer.id) {
                 this.dataValidation()
-
-
-
+                this.checkMatchNumber()
+            }
+        }
     }
 
-    //when the btn is clicked we want to check if the num = random and validate
+
+    fun checkMatchNumber(){
+        //check if the generated num == number added by user
+        if (edtAnswer.text.toString().toInt() == correctNumber){
+            //alert showing if he wins winner
+            val alertBuilder = AlertDialog.Builder(this)
+            Toast.makeText(this, "Correct. You win", Toast.LENGTH_LONG).show()
+
+            alertBuilder.setTitle("Result")
+            alertBuilder.setMessage("Great! You won the game. Do You have to play again ?")
+            alertBuilder.setPositiveButton("PLAY AGAIN"){ dialog, which ->
+
+            }
+
+            alertBuilder.setNegativeButton("EXIT GAME"){ dialog, which ->
+
+            }
+
+
+        }
+        else if (edtAnswer.text.toString().toInt() > correctNumber){
+            Toast.makeText(this,"The number that you have entered is greater" ,
+            Toast.LENGTH_SHORT).show()
+        }
+        else if (edtAnswer.text.toString().toInt() < correctNumber){
+            Toast.makeText(this,"The number that you have entered is less than the correct answer" ,
+                Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+
 
 }
